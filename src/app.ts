c99bbaf28201f410fa2d679342da8309d8a2548a
Nathan <nathanverdier34@gmail.com>
2024-04-04 00:10:50 +0200

    :construction: Add: Exemple route and middleware

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,40 @@ const app = express();
 // Log HTTP requests
 app.use(morgan("dev"));
 
+
+const { auth, requiredScopes } = require('express-oauth2-jwt-bearer');
+
+// auth0 middleware
+const checkJwt = auth({
+    audience: '{yourApiIdentifier}',
+    issuerBaseURL: `https://{yourDomain}/`,
+  });
+  
+
+// exemple d'utilisation des routes avec auth0
+
+// // This route doesn't need authentication
+// app.get('/api/public', function(req, res) {
+//     res.json({
+//       message: 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'
+//     });
+//   });
+  
+//   // This route needs authentication
+//   app.get('/api/private', checkJwt, function(req, res) {
+//     res.json({
+//       message: 'Hello from a private endpoint! You need to be authenticated to see this.'
+//     });
+//   });
+  
+//   const checkScopes = requiredScopes('read:messages');
+  
+//   app.get('/api/private-scoped', checkJwt, checkScopes, function(req, res) {
+//     res.json({
+//       message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'
+//     });
+//   });
+
 // Add CORS headers.
 app.use(
     cors({
