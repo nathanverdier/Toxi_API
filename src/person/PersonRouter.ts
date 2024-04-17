2d9592ceb59490e67e04a6e523465891bce51c17
Nathan <nathanverdier34@gmail.com>
2024-04-17 20:17:02 +0200

    ♻️ Refactor: PersonRouter to use auth0

--- src/person/PersonRouter.ts
+++ src/person/PersonRouter.ts
@@ -1,17 +1,22 @@
 import express from "express";
 import { personService } from "./PersonService";
+import checkJwt from "../app"; // importez le middleware d'authentification
 
 const PersonRouter = express.Router();
 
 PersonRouter.route('/')
-    .get(async (_, res) => {
-        const persons = await personService.getAll();
-        console.log(persons);
-        res.json(persons);
+    .get(checkJwt, async (_, res) => { // utilisez le middleware d'authentification
+        try {
+            const persons = await personService.getAll();
+            console.log(persons);
+            res.json(persons);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Une erreur est survenue lors de la récupération des personnes.' });
+        }
     })
-    .post((_, res) => {
+    .post(checkJwt, (_, res) => { // utilisez le middleware d'authentification
         res.json("coucou post");
     });
 
 export default PersonRouter;
-
