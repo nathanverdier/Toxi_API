9bd4d4c76b9c4f4edc782ccd6ea292ed356abb14
Nathan <nathanverdier34@gmail.com>
2024-06-09 19:36:34 +0200

    ✨ Add : functionality to add a new person in PersonService

--- src/person/PersonService.ts
+++ src/person/PersonService.ts
@@ -1,5 +1,10 @@
+import PersonModel from "./PersonModel";
 import Person from "./PersonSchema";
 
 export const personService = {
     getAll: async () => await Person.find(),
+    addSomeone: async (newPerson: PersonModel) => {
+        const person = new Person(newPerson);
+        return await person.save();
+    },
 };
