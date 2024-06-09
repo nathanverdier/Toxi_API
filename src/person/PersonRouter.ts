df215274c2d42a4cf8a575d93c60fb05c335da1f
Nathan <nathanverdier34@gmail.com>
2024-06-09 19:36:21 +0200

    ✨ Add: PersonRouter for adding a new person

--- src/person/PersonRouter.ts
+++ src/person/PersonRouter.ts
@@ -17,8 +17,15 @@ PersonRouter.route('/')
             res.status(500).json({ message: 'Une erreur est survenue lors de la récupération des personnes.' });
         }
     })
-    .post(checkJwt, (_, res) => {
-        res.json("coucou post");
+    .post(checkJwt, async (req, res) => {
+        try {
+            const newPerson = req.body;
+            const addedPerson = await personService.addSomeone(newPerson);
+            res.json(addedPerson);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Une erreur est survenue lors de l\'ajout de la personne.' });
+        }
     });
 
 export default PersonRouter;
