bcc32433e514541deca7ff952f35d72f1cf16316
Nathan <nathanverdier34@gmail.com>
2024-06-10 10:25:31 +0200

    ✨ Update : app.ts to include body-parser middleware

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,12 @@ import { errorHandler } from "./utils/errorHandler";
 import { notFoundHandler } from "./utils/notFoundHandler";
 import { authErrorHandler } from "./utils/authErrorHandler";
 import { bodyParserErrorHandler } from "./utils/bodyParserErrorHandler";
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 // Log HTTP requests
 app.use(morgan("tiny"));
